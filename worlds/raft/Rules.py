20d0aa0130ac94308aa9f6a50d0212d0b7087a8b
SunnyBat <Sunnybat@yahoo.com>
2021-12-23 00:58:06 -0800

    Add rules for Research Table item recipes

--- worlds/raft/Rules.py
+++ worlds/raft/Rules.py
@@ -4,19 +4,42 @@ from .Regions import regionMap
 from ..AutoWorld import LogicMixin
 
 class RaftLogic(LogicMixin):
-    def can_produce_metals(self, player):
+    def can_smelt_items(self, player):
         return self.has("Placeable_CookingStand_Smelter", player)
+
+    def can_craft_bolt(self, player):
+        return self.can_smelt_items(player) and self.has("Bolt", player)
+
+    def can_craft_hinge(self, player):
+        return self.can_smelt_items(player) and self.has("Hinge", player)
+
+    def can_craft_circuitBoard(self, player):
+        return self.can_smelt_items(player) and self.has("CircuitBoard", player)
+
+    def can_craft_plasticBottle(self, player):
+        return self.can_smelt_items(player) and self.has("PlasticBottle_Empty", player)
+
+    def can_craft_shears(self, player):
+        return self.can_smelt_items(player) and self.can_craft_hinge(player) and self.has("Shear", player)
+
+    def can_get_dirt(self, player):
+        return self.has("Shovel", player)
+
+    def can_capture_animals(self, player):
+        return (self.can_smelt_items(player) and self.can_craft_bolt(player) # Required for crafting
+            and self.has("NetCanister", player) and self.has("NetGun", player)
+            and self.has("Placeable_Cropplot_Grass", player) and self.can_get_dirt(player))
     
     def has_game_basics(self, player):
-        return self.has("Bolt", player) and self.has("Hinge", player)
+        return self.can_craft_bolt(player) and self.can_craft_hinge(player)
 
     def can_navigate(self, player): # Sail is added by default and not considered in Archipelago
-        return (self.can_produce_metals(player) and self.has_game_basics(player)
+        return (self.can_smelt_items(player) and self.has_game_basics(player)
             and self.has("Battery", player) and self.has("CircuitBoard", player)
             and self.has("Placeable_Reciever", player) and self.has("Placeable_Reciever_Antenna", player))
 
     def can_drive(self, player): # The player can go wherever they want with the engine
-        return (self.can_produce_metals(player) and self.has_game_basics(player)
+        return (self.can_smelt_items(player) and self.has_game_basics(player)
             and self.has("Placeable_MotorWheel", player) and self.has("Placeable_SteeringWheel", player))
 
     def can_access_radio_tower(self, player):
@@ -51,7 +74,6 @@ class RaftLogic(LogicMixin):
     def can_complete_tangaroa(self, player):
         return self.can_access_tangaroa(player)
 
-
 def set_rules(world, player):
     # Map region to check to see if we can access it
     regionChecks = {
@@ -68,6 +90,36 @@ def set_rules(world, player):
         "Tangaroa": lambda state: state.can_access_tangaroa(player),
         "TangaroaCompletion": lambda state: state.can_complete_tangaroa(player)
     }
+    itemChecks = {
+        "Plank": lambda state: True,
+        "Plastic": lambda state: True,
+        "Clay": lambda state: True,
+        "Stone": lambda state: True,
+        "Rope": lambda state: True,
+        "Nail": lambda state: True,
+        "Scrap": lambda state: True,
+        "Leather": lambda state: True, # Wiki has conflicting information on whether Large Islands before Radio Tower has been visited; pretty sure they can
+        "SeaVine": lambda state: True,
+        "Brick_Dry": lambda state: True,
+        "Feather": lambda state: True,
+        "Thatch": lambda state: True, # Palm Leaf
+        "Placeable_GiantClam": lambda state: True,
+        "MetalIngot": lambda state: state.can_smelt_items(player),
+        "CopperIngot": lambda state: state.can_smelt_items(player),
+        "VineGoo": lambda state: state.can_smelt_items(player),
+        "ExplosivePowder": lambda state: state.can_smelt_items(player),
+        "Glass": lambda state: state.can_smelt_items(player),
+        "Bolt": lambda state: state.can_craft_bolt(player),
+        "Hinge": lambda state: state.can_craft_hinge(player),
+        "CircuitBoard": lambda state: state.can_craft_circuitBoard(player),
+        "PlasticBottle_Empty": lambda state: state.can_craft_plasticBottle(player),
+        "Shear": lambda state: state.can_craft_shears(player),
+        "Wool": lambda state: state.can_capture_animals(player) and state.can_craft_shears(player),
+        "HoneyComb": lambda state: state.can_access_balboa_island(player),
+        "Jar_Bee": lambda state: state.can_access_balboa_island(player) and state.can_smelt_items(player),
+        "Dirt": lambda state: state.can_get_dirt(player),
+        "Egg": lambda state: state.can_capture_animals(player)
+    }
 
     # Location rules
     for region in regionMap:
@@ -78,7 +130,20 @@ def set_rules(world, player):
      
     # Process locations
     for location in location_table:
-        set_rule(world.get_location(location["name"], player), regionChecks[location["region"]])
+        locFromWorld = world.get_location(location["name"], player)
+        if "requiresAccessToItems" in location:
+            def ruleLambda(state):
+                canAccess = regionChecks[location["region"]](state)
+                for item in locFromWorld["requiresAccessToItems"]:
+                    if not itemChecks[item](state):
+                        canAccess = False
+                        break
+                return canAccess
+            set_rule(locFromWorld, ruleLambda)
+        else:
+            set_rule(locFromWorld, regionChecks[location["region"]])
 
     # Victory location
     world.completion_condition[player] = lambda state: state.has('Victory', player)
+
+    # Crafting Table requirements
