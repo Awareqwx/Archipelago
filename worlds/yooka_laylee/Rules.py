a90e5962b7ec023a5a6da4cc48b0fd09703abbae
SunnyBat <Sunnybat@yahoo.com>
2023-08-21 11:09:15 -0700

    Better rules to fix bugs

--- worlds/yooka_laylee/Rules.py
+++ worlds/yooka_laylee/Rules.py
@@ -8,29 +8,55 @@ import pkgutil
 cashinotokens_table = json.loads(pkgutil.get_data(__name__, "cashinotokens.json").decode())
 
 class YookaLogic(LogicMixin):
-    def yooka_can_access_tropics(self, player): #Once abilities are items, this will require Reptile Roll
-        return self.has("Pagie", player, 1)
+    def yooka_can_access_tropics(self, state, player):
+        return (self.has("Pagie", player, 1)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and (self.has("Reptile Roll", player) or self.has("Flappy Flight", player)))
 
-    def yooka_can_access_tropics_exp(self, player):
-        return self.has("Pagie", player, 4)
+    def yooka_can_access_tropics_exp(self, state, player):
+        return self.has("Pagie", player, 4) and self.yooka_can_access_tropics(state, player)
 
-    def yooka_can_access_glacier(self, player): #Once abilities are items, this will require Glide
-        return self.has("Pagie", player, 7)
+    def yooka_can_access_glacier(self, state, player):
+        return (self.has("Pagie", player, 7)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and (self.has("Reptile Roll", player) or self.has("Flappy Flight", player)) and self.has("Buddy Slam", player))
 
-    def yooka_can_access_glacier_exp(self, player):
-        return self.has("Pagie", player, 12)
+    def yooka_can_access_glacier_exp(self, state, player):
+        return self.has("Pagie", player, 12) and self.yooka_can_access_glacier(state, player)
 
-    def yooka_can_access_marsh(self, player): #Once abilities are items, this will require Buddy Bubble
-        return self.has("Pagie", player, 19)
+    def yooka_can_access_marsh(self, state, player):
+        return (self.has("Pagie", player, 19)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and (self.has("Reptile Roll", player) or self.has("Flappy Flight", player)) and self.has("Buddy Slam", player)
+                and self.has("Slurp Shot", player) and self.has("Buddy Bubble", player))
 
-    def yooka_can_access_marsh_exp(self, player):
-        return self.has("Pagie", player, 27)
+    def yooka_can_access_marsh_exp(self, state, player):
+        return self.has("Pagie", player, 27) and self.yooka_can_access_marsh(state, player)
 
-    def yooka_can_access_cashino(self, player): #Once abilities are items, this will require Camo Cloak
-        return self.has("Pagie", player, 37)
+    def yooka_can_access_cashino(self, state, player):
+        return (self.has("Pagie", player, 37)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and (self.has("Reptile Roll", player) or self.has("Flappy Flight", player)) and self.has("Buddy Slam", player)
+                and self.has("Slurp Shot", player) and self.has("Buddy Bubble", player) and self.has("Camo Cloak", player))
 
-    def yooka_can_access_cashino_exp(self, player):
-        return self.has("Pagie", player, 48)
+    def yooka_can_access_cashino_exp(self, state, player):
+        return self.has("Pagie", player, 48) and self.yooka_can_access_cashino(state, player)
+
+    def yooka_can_access_galaxy(self, state, player): #Once abilities are items, this will require Flappy Flight
+        return (self.has("Pagie", player, 60)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and self.has("Flappy Flight", player) and self.has("Buddy Slam", player) and self.has("Slurp Shot", player)
+                and self.has("Buddy Bubble", player))
+
+    def yooka_can_access_galaxy_exp(self, state, player):
+        return self.has("Pagie", player, 75) and self.yooka_can_access_galaxy(state, player)
+
+    def yooka_can_access_end(self, state, player): # Technically don't need Tail Twirl and Sonar Shield, but the final boss is pretty miserable without it
+        return (self.has("Pagie", player, 100)
+                and self.yooka_has_ability(state, player, "<DamagingAbility>")
+                and self.has("Flappy Flight", player) and self.has("Buddy Slam", player) and self.has("Slurp Shot", player)
+                and self.has("Buddy Bubble", player) and self.has("Tail Twirl", player) and self.has("Reptile Roll", player)
+                and self.has("Sonar Shield", player))
 
     def yooka_can_get_cashino_token_count(self, state, player, tokenCount):
         accessibleTokenCount = 0
@@ -41,15 +67,6 @@ class YookaLogic(LogicMixin):
             else:
                 accessibleTokenCount += tokenGroup["count"]
         return accessibleTokenCount >= tokenCount
-
-    def yooka_can_access_galaxy(self, player): #Once abilities are items, this will require Flappy Flight
-        return self.has("Pagie", player, 60)
-
-    def yooka_can_access_galaxy_exp(self, player):
-        return self.has("Pagie", player, 75)
-
-    def yooka_can_access_end(self, player):
-        return self.has("Pagie", player, 100)
 
     def yooka_has_requirements(self, state, player, requirements, searchMode = 0):
         if isinstance(requirements, str): # Is ability name
@@ -83,20 +100,20 @@ class YookaLogic(LogicMixin):
                 or self.yooka_has_ability(state, player, "Reptile Rush")
                 or self.yooka_has_ability(state, player, "Sonar Shield")
             ),
-            "<TribalstackPagie>": lambda state: self.yooka_can_access_tropics(player), # Wrecked Crow's Nest doesn't spawn until after Tropics entered
-            "<ExpandedTribalstackTropics>": lambda state: self.yooka_can_access_tropics_exp(player), # Non-expanded pagie but with different options in expansion
+            "<TribalstackPagie>": lambda state: self.yooka_can_access_tropics(state, player), # Wrecked Crow's Nest doesn't spawn until after Tropics entered
+            "<ExpandedTribalstackTropics>": lambda state: self.yooka_can_access_tropics_exp(state, player), # Non-expanded pagie but with different options in expansion
             "<GlacierUpperAccess>": lambda state: (
                 self.has("Flappy Flight", player)
                 or (self.has("Slurp State", player) and (self.has("Tail Twirl", player) or self.has("Glide", player)))
             ),
-            "<GlacierLowerAccess>": lambda state: self.has("Buddy Slam", player) and self.has("Sonar Shot", player) and self.yooka_can_access_tropics(state, player, "Reptile Rush"),
+            "<GlacierLowerAccess>": lambda state: self.has("Buddy Slam", player) and self.has("Sonar Shot", player) and self.yooka_has_ability(state, player, "Reptile Rush"),
             "<GlacierBoss>": lambda state: (
                 (self.yooka_has_ability(state, player, "<GlacierUpperAccess>") or self.yooka_has_ability(state, player, "<GlacierLowerAccess>"))
                 and self.has("Buddy Slam", player) and self.has("Slurp Shot", player)
             ),
             "<MoodymazeEntry>": lambda state: self.has("Bubble Buddy", player) or self.has("Lizard Lash", player) or self.has("Flappy Flight", player),
             "<CashinoEntry>": lambda state: self.has("Camo Cloak", player) or self.has("Flappy Flight", player),
-            "<ExpandedCashino>": lambda state: self.yooka_can_access_cashino_exp(player), # Specifically for Cashino tokens requirements
+            "<ExpandedCashino>": lambda state: self.yooka_can_access_cashino_exp(state, player), # Specifically for Cashino tokens requirements
             "<GalaxyEntry>": lambda state: self.has("Flappy Flight", player) or self.has("Glide", player) or self.has("Health Booster", player, 5)
         }
         if ability in specialRequirements:
@@ -122,17 +139,17 @@ def set_rules(world, player):
         "Hivory Towers Waterworks": lambda state: state.yooka_has_ability(state, player, "<DamagingAbility>"),
         "Hivory Towers Outside (NoFlight)": lambda state: state.yooka_has_ability(state, player, "<DamagingAbility>"),
         "Hivory Towers Outside (Flight)": lambda state: state.yooka_has_ability(state, player, "<DamagingAbility>"),
-        "Tribalstack Tropics": lambda state: state.yooka_can_access_tropics(player),
-        "Expanded Tropics": lambda state: state.yooka_can_access_tropics_exp(player),
-        "Glitterglaze Glacier": lambda state: state.yooka_can_access_glacier(player),
-        "Expanded Glacier": lambda state: state.yooka_can_access_glacier_exp(player),
-        "Moodymaze Marsh": lambda state: state.yooka_can_access_marsh(player),
-        "Expanded Marsh": lambda state: state.yooka_can_access_marsh_exp(player),
-        "Capital Cashino": lambda state: state.yooka_can_access_cashino(player),
-        "Expanded Cashino": lambda state: state.yooka_can_access_cashino_exp(player),
-        "Galleon Galaxy": lambda state: state.yooka_can_access_galaxy(player),
-        "Expanded Galaxy": lambda state: state.yooka_can_access_galaxy_exp(player),
-        "Endgame": lambda state: state.yooka_can_access_end(player)
+        "Tribalstack Tropics": lambda state: state.yooka_can_access_tropics(state, player),
+        "Expanded Tropics": lambda state: state.yooka_can_access_tropics_exp(state, player),
+        "Glitterglaze Glacier": lambda state: state.yooka_can_access_glacier(state, player),
+        "Expanded Glacier": lambda state: state.yooka_can_access_glacier_exp(state, player),
+        "Moodymaze Marsh": lambda state: state.yooka_can_access_marsh(state, player),
+        "Expanded Marsh": lambda state: state.yooka_can_access_marsh_exp(state, player),
+        "Capital Cashino": lambda state: state.yooka_can_access_cashino(state, player),
+        "Expanded Cashino": lambda state: state.yooka_can_access_cashino_exp(state, player),
+        "Galleon Galaxy": lambda state: state.yooka_can_access_galaxy(state, player),
+        "Expanded Galaxy": lambda state: state.yooka_can_access_galaxy_exp(state, player),
+        "Endgame": lambda state: state.yooka_can_access_end(state, player)
     }
 
     # Region access rules
