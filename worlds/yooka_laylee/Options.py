d3b98d4579a2191c89b79cbb2c0798811521a2bb
SunnyBat <Sunnybat@yahoo.com>
2023-08-21 18:18:23 -0700

    Add docs, slightly better option names

--- worlds/yooka_laylee/Options.py
+++ worlds/yooka_laylee/Options.py
@@ -2,6 +2,7 @@ from Options import Range, Toggle, DefaultOnToggle, Choice, DeathLink
 
 class ForceLocalFirstItem(Toggle):
     """If enabled, forces an ability that can deal damage into your world as the first location you can check (where Trowzer teaches you Tail Twirl). Otherwise, it will be filled into any location that is reachable from the beginning of any game."""
+    display_name = "Force local first abilitiy"
 
 class PreventTropicsBK(DefaultOnToggle):
     """If enabled, forces either Reptile Roll or Flappy Flight into your world in a location accessible before Tribalstack Tropics. If disabled, these abilities may be placed anywhere."""
@@ -10,8 +11,8 @@ class PreventTropicsBK(DefaultOnToggle):
 class FlappyFlightLocation(Choice):
     """Choose where Flappy Flight is put. Flappy Flight opens up many checks and effectively invalidates some abilities like Lizard Leap. "Early" refers to one of the earlygame checks before entering Tribalstack Tropics."""
     display_name = "Flappy Flight location"
-    option_forceEarly = 1
-    option_allowEarly = 2
+    option_force_early = 1
+    option_allow_early = 2
     option_vanilla = 3
     option_anywhere = 4
     default = 2
